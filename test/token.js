fb9bbc953f9d870c157f6e998fb0343d82c9a222
eL <68808205+love-droid@users.noreply.github.com>
2022-07-14 21:20:56 +0530

    Transfer tokens testing

--- test/token.js
+++ test/token.js
@@ -8,14 +8,37 @@ describe("Token contract",function(){
 
         const [owner] = await ethers.getSigners();
 
-        console.log("Signers object",owner);
+        
         const Token = await ethers.getContractFactory("Token"); //instance contract
 
         const hardhatToken = await Token.deploy(); // deploy contract 
 
         const ownerBalance = await hardhatToken.balanceOf(owner.address); // ownerBalance  = 10000
-        console.log("Owner Address:", owner.address);
+       
 
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance); // total supply = 10000
-    }) 
+    });
+    it("Transfer tokens b/w accounts",async function(){
+
+        const [owner,add1,add2] = await ethers.getSigners();
+
+        
+        const Token = await ethers.getContractFactory("Token"); //instance contract
+
+        const hardhatToken = await Token.deploy(); // deploy contract 
+        // transfer 10 tokens from owner to add1
+
+        await hardhatToken.transfer(add1.address,10);
+
+        expect(await hardhatToken.balanceOf(add1.address)).to.equal(10);
+
+        // transfer 5 tokens from add1 to add2
+        await  hardhatToken.connect(add1).transfer(add2.address,5);
+
+        expect(await hardhatToken.balanceOf(add2.address)).to.equal(5);
+        
+
+
+     
+    })  
 })
