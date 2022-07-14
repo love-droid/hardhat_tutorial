0005364e22f119b71d1e57e51e26185b3d8602af
eL <68808205+love-droid@users.noreply.github.com>
2022-07-14 20:50:17 +0530

    testing

---
+++ test/token.js
@@ -0,0 +1,21 @@
+const {expect} = require("chai");
+
+const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
+
+describe("Token contract",function(){
+
+    it("Deployment should assign the total supply of tokens to the owner",async function(){
+
+        const [owner] = await ethers.getSigners();
+
+        console.log("Signers object",owner);
+        const Token = await ethers.getContractFactory("Token"); //instance contract
+
+        const hardhatToken = await Token.deploy(); // deploy contract 
+
+        const ownerBalance = await hardhatToken.balanceOf(owner.address); // ownerBalance  = 10000
+        console.log("Owner Address:", owner.address);
+
+        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance); // total supply = 10000
+    }) 
+})
